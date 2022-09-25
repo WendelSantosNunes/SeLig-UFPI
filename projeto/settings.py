e505a1ea07d4965a0454cd8947f43c92247d1476
Wendel dos Santos Nunes <wendelnunes9999@gmail.com>
2022-09-25 00:01:32 -0300

    Update project

--- projeto/settings.py
+++ projeto/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'project',
     'conta.apps.ContaConfig',
     'cadastro.apps.CadastroConfig',
     'publicacao.apps.PublicacaoConfig',
